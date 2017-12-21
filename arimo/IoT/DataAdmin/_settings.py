e027ae4e3cd6c66691be75c1c0c787e22f833c89
Vinh Luong <MBALearnsToCode@UChicago.edu>
2017-12-21 12:50:55 -0800

    set secret key to _ temporarily

--- arimo/IoT/DataAdmin/_settings.py
+++ arimo/IoT/DataAdmin/_settings.py
@@ -27,7 +27,7 @@ _ON_LINUX_CLUSTER = sys.platform.startswith('linux')
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '...'
+SECRET_KEY = '_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not _ON_LINUX_CLUSTER
