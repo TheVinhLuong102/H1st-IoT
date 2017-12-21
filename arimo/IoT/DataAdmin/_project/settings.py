38b94eb8b6cb1cb29e78b2ca3598c83a67a3f513
Vinh Luong <MBALearnsToCode@UChicago.edu>
2017-12-21 12:59:43 -0800

    create arimo.IoT.DataAdmin._project

--- arimo/IoT/DataAdmin/_project/settings.py
+++ arimo/IoT/DataAdmin/_project/settings.py
@@ -20,7 +20,7 @@ _ON_LINUX_CLUSTER = sys.platform.startswith('linux')
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,7 +79,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
-# ROOT_URLCONF = '...'
+ROOT_URLCONF = 'arimo.IoT.DataAdmin._urls'
 
 TEMPLATES = [
     {
@@ -151,6 +151,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
