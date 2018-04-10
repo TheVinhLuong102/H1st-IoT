2d35f96a9f35b3e38005cbfb73f65019da42501d
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-04-10 02:52:14 -0700

    update arimo.IoT.DataAdmin.PredMaint.admin

--- arimo/IoT/DataAdmin/PredMaint/admin.py
+++ arimo/IoT/DataAdmin/PredMaint/admin.py
@@ -4,8 +4,8 @@ from .models import Blueprint
 
 
 class BlueprintAdmin(ModelAdmin):
-    list_display = 'url', 'active'
-    list_filter = 'active',
+    list_display = 'url', 'timestamp', 'active'
+    list_filter = 'active', 'timestamp'
     search_fields = 'url',
 
 
