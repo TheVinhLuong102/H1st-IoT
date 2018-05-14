e110d9477370a6fa229719e6fcfd43658f139e5f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-05-14 14:45:40 -0700

    refactor docs/parse-docstr

--- docs/parse-docstr.sh
+++ docs/parse-docstr.sh
@@ -3,10 +3,20 @@
 # this scripts needs to be called from the Project's root dir
 
 
+MODULE=arimo
+DOCS_DIR=docs
+
+
 # generate .rst files from module code & docstrings
 # any pathnames given at the end are paths to be excluded ignored during generation.
 # ref: http://www.sphinx-doc.org/en/master/man/sphinx-apidoc.html
-sphinx-apidoc --force --maxdepth 4 --module-first --output-dir docs --separate arimo \
-    arimo/IoT/DataAdmin/_project \
-    arimo/IoT/DataAdmin/base \
-    arimo/IoT/DataAdmin/PredMaint
+sphinx-apidoc --force --maxdepth 4 --module-first --output-dir $DOCS_DIR --separate $MODULE \
+    $MODULE/IoT/DataAdmin/_project \
+    $MODULE/IoT/DataAdmin/base \
+    $MODULE/IoT/DataAdmin/PredMaint
+
+
+# get rid of undocumented members
+# grep -C2 ":undoc-members:" $DOCS_DIR/$MODULE*.rst
+sed -e /:undoc-members:/d -i .orig $DOCS_DIR/$MODULE*.rst
+rm $DOCS_DIR/*.orig
