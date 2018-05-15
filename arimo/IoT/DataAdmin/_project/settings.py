de5499bdf1124f9dd24f9dfad11709110d041bc6
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-05-15 11:22:15 -0700

    refactor arimo.IoT.DataAdmin scripts

--- arimo/IoT/DataAdmin/_project/settings.py
+++ arimo/IoT/DataAdmin/_project/settings.py
@@ -119,9 +119,13 @@ WSGI_APPLICATION = 'arimo.IoT.DataAdmin._project.wsgi.application'
 _DB_DETAILS_FILE_NAME = 'db.yaml'
 _DB_DETAILS_FILE_PATH = os.path.join(_PROJECT_DIR, _DB_DETAILS_FILE_NAME)
 
-DATABASES = {
-    'default': yaml.safe_load(open(_DB_DETAILS_FILE_PATH, 'r'))
-}
+_db_details = yaml.safe_load(stream=open(_DB_DETAILS_FILE_PATH, 'r'))['db']['admin']
+
+DATABASES = \
+    dict(default=
+        dict(ENGINE='django.db.backends.postgresql', PORT='5432',
+             HOST=_db_details['host'], NAME=_db_details['db_name'],
+             USER=_db_details['user'], PASSWORD=_db_details['password']))
 
 
 # Password validation
--- deploy.py
+++ deploy.py
@@ -1,13 +1,15 @@
 import argparse
 import os
 from ruamel import yaml
+import shutil
 
 from arimo.IoT.DataAdmin import Project, _YAML_EXT
 from arimo.IoT.DataAdmin._project.settings import _DB_DETAILS_FILE_PATH
 
 
-_DEFAULT_IoT_DATA_ADMIN_DB_DETAILS = \
-    yaml.safe_load(open(_DB_DETAILS_FILE_PATH, 'r'))
+IoT_DATA_ADMIN_DB_DETAILS_ORIG_FILE_PATH = _DB_DETAILS_FILE_PATH + '.orig'
+
+assert not os.path.isfile(IoT_DATA_ADMIN_DB_DETAILS_ORIG_FILE_PATH)
 
 
 arg_parser = argparse.ArgumentParser()
@@ -18,37 +20,30 @@ arg_parser.add_argument('--project', default='TEST')
 args = arg_parser.parse_args()
 
 
+iot_dataadmin_db_details_file_path = \
+    os.path.join(
+        Project.CONFIG_DIR_PATH,
+        args.project + _YAML_EXT)
+
 iot_dataadmin_db_details = \
-    yaml.safe_load(
-        open(os.path.join(
-                Project.CONFIG_DIR_PATH,
-                args.project + _YAML_EXT),
-             'r'))['db']['admin']
+    yaml.safe_load(stream=os.path.isfile(iot_dataadmin_db_details_file_path))['db']['admin']
+
+assert iot_dataadmin_db_details['host'] \
+   and iot_dataadmin_db_details['db_name'] \
+   and iot_dataadmin_db_details['user'] \
+   and iot_dataadmin_db_details['password']
 
 
-yaml.safe_dump(
-    data=dict(
-        ENGINE=_DEFAULT_IoT_DATA_ADMIN_DB_DETAILS['ENGINE'],
-        HOST=iot_dataadmin_db_details['host'],
-        PORT=_DEFAULT_IoT_DATA_ADMIN_DB_DETAILS['PORT'],
-        NAME=iot_dataadmin_db_details['name'],
-        USER=iot_dataadmin_db_details['user'],
-        PASSWORD=iot_dataadmin_db_details['password']),
-    stream=open(_DB_DETAILS_FILE_PATH, 'w'),
-    default_style=None,
-    default_flow_style=False,   # collections to be always serialized in the block style
-    canonical=None,
-    indent=2,
-    width=None,
-    allow_unicode=True,
-    line_break=None,
-    encoding=u'utf-8',
-    explicit_start=None,
-    explicit_end=None,
-    version=None,
-    tags=None,
-    block_seq_indent=None)
+shutil.copyfile(
+    src=_DB_DETAILS_FILE_PATH,
+    dst=IoT_DATA_ADMIN_DB_DETAILS_ORIG_FILE_PATH)
 
+assert os.path.isfile(IoT_DATA_ADMIN_DB_DETAILS_ORIG_FILE_PATH)
+
+
+shutil.copyfile(
+    src=iot_dataadmin_db_details_file_path,
+    dst=_DB_DETAILS_FILE_PATH)
 
 os.system(
     'eb {} {}'.format(
@@ -56,19 +51,10 @@ os.system(
         args.project))
 
 
-yaml.safe_dump(
-    data=_DEFAULT_IoT_DATA_ADMIN_DB_DETAILS,
-    stream=open(_DB_DETAILS_FILE_PATH, 'w'),
-    default_style=None,
-    default_flow_style=False,   # collections to be always serialized in the block style
-    canonical=None,
-    indent=2,
-    width=None,
-    allow_unicode=True,
-    line_break=None,
-    encoding=u'utf-8',
-    explicit_start=None,
-    explicit_end=None,
-    version=None,
-    tags=None,
-    block_seq_indent=None)
+shutil.copyfile(
+    src=IoT_DATA_ADMIN_DB_DETAILS_ORIG_FILE_PATH,
+    dst=_DB_DETAILS_FILE_PATH)
+
+os.remove(path=IoT_DATA_ADMIN_DB_DETAILS_ORIG_FILE_PATH)
+
+assert not os.path.isfile(IoT_DATA_ADMIN_DB_DETAILS_ORIG_FILE_PATH)
