a29b713cb21585adc672657666d3931f1bf1808c
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-05-15 22:57:24 -0700

    enhance arimo.IoT.DataAdmin.Project.equipment_general_type(...)

--- arimo/IoT/DataAdmin/__init__.py
+++ arimo/IoT/DataAdmin/__init__.py
@@ -139,7 +139,7 @@ class Project(object):
     def equipment_general_type(self, equipment_general_type_name):
         equipment_general_types = \
             self.data.EquipmentGeneralTypes.filter(
-                name=equipment_general_type_name)
+                name=clean_lower_str(equipment_general_type_name))
 
         assert len(equipment_general_types) == 1
 
