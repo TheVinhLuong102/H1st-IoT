65dfab570133cf33fb65ad8fa41991d9fc48e64a
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-09-07 18:42:29 -0700

    enhance arimo.IoT.DataAdmin.Project

--- arimo/IoT/DataAdmin/__init__.py
+++ arimo/IoT/DataAdmin/__init__.py
@@ -1,6 +1,7 @@
 from collections import Counter
 import datetime
 import os
+import pandas
 from ruamel import yaml
 import six
 
@@ -729,9 +730,73 @@ class Project(object):
             return self.data.EquipmentInstances.filter(
                     name=clean_lower_str(equipment_instance_name))
 
-    def _equipment_data_fields_n_equipment_unique_types(only0=True):
+    def _equipment_general_types_n_equipment_data_fields(self, only0=True):
         df = pandas.DataFrame.from_records(
-            data=IoT_DATA_ADMIN_PROJECT.models.base.EquipmentDataField.objects
+            data=self.data.EquipmentGeneralTypes
+                .values('name')
+                .annotate(n_equipment_data_fields=Count('equipment_data_field'))
+                .order_by('name'),
+            index=None,
+            exclude=None,
+            columns=['name', 'n_equipment_data_fields'],
+            coerce_float=False,
+            nrows=None)
+
+        return df.loc[df.n_equipment_data_fields == 0] \
+            if only0 \
+          else df
+
+    def _equipment_general_types_n_equipment_unique_type_groups(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentGeneralTypes
+                .values('name')
+                .annotate(n_equipment_unique_type_groups=Count('equipment_unique_type_group'))
+                .order_by('name'),
+            index=None,
+            exclude=None,
+            columns=['name', 'n_equipment_unique_type_groups'],
+            coerce_float=False,
+            nrows=None)
+
+        return df.loc[df.n_equipment_unique_type_groups == 0] \
+            if only0 \
+          else df
+
+    def _equipment_general_types_n_equipment_unique_types(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentGeneralTypes
+                .values('name')
+                .annotate(n_equipment_unique_types=Count('equipment_unique_type'))
+                .order_by('name'),
+            index=None,
+            exclude=None,
+            columns=['name', 'n_equipment_unique_types'],
+            coerce_float=False,
+            nrows=None)
+
+        return df.loc[df.n_equipment_unique_types == 0] \
+            if only0 \
+          else df
+
+    def _equipment_general_types_n_equipment_instances(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentGeneralTypes
+                .values('name')
+                .annotate(n_equipment_instances=Count('equipment_instance'))
+                .order_by('name'),
+            index=None,
+            exclude=None,
+            columns=['name', 'n_equipment_instances'],
+            coerce_float=False,
+            nrows=None)
+
+        return df.loc[df.n_equipment_instances == 0] \
+            if only0 \
+          else df
+
+    def _equipment_data_fields_n_equipment_unique_types(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentDataFields
                 .values('equipment_general_type__name', 'equipment_data_field_type__name', 'name')
                 .annotate(n_equipment_unique_types=Count('equipment_unique_types'))
                 .order_by('equipment_general_type__name', 'equipment_data_field_type__name', 'name'),
@@ -743,9 +808,57 @@ class Project(object):
 
         return df.loc[df.n_equipment_unique_types == 0] \
             if only0 \
-            else df
+          else df
 
-    def equipment_unique_types_n_equipment_data_fields(only0=True):
+    def _equipment_data_fields_n_equipment_instances(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentDataFields
+                .values('equipment_general_type__name', 'equipment_data_field_type__name', 'name')
+                .annotate(n_equipment_instances=Count('equipment_instance'))
+                .order_by('equipment_general_type__name', 'equipment_data_field_type__name', 'name'),
+            index=None,
+            exclude=None,
+            columns=['equipment_general_type__name', 'equipment_data_field_type__name', 'name', 'n_equipment_instances'],
+            coerce_float=False,
+            nrows=None)
+
+        return df.loc[df.n_equipment_instances == 0] \
+            if only0 \
+          else df
+
+    def _equipment_unique_type_groups_n_equipment_unique_types(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentUniqueTypeGroups
+                .values('equipment_general_type__name', 'name')
+                .annotate(n_equipment_unique_types=Count('equipment_unique_types'))
+                .order_by('equipment_general_type__name', 'name'),
+            index=None,
+            exclude=None,
+            columns=['equipment_general_type__name', 'name', 'n_equipment_unique_types'],
+            coerce_float=False,
+            nrows=None)
+
+        return df.loc[df.n_equipment_unique_types == 0] \
+            if only0 \
+          else df
+
+    def _equipment_unique_types_n_equipment_unique_type_groups(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentUniqueTypes
+                .values('equipment_general_type__name', 'name')
+                .annotate(n_equipment_unique_type_groups=Count('groups'))
+                .order_by('equipment_general_type__name', 'name'),
+            index=None,
+            exclude=None,
+            columns=['equipment_general_type__name', 'name', 'n_equipment_unique_type_groups'],
+            coerce_float=False,
+            nrows=None)
+
+        return df.loc[df.n_equipment_unique_type_groups == 0] \
+            if only0 \
+          else df
+
+    def _equipment_unique_types_n_equipment_data_fields(self, only0=True):
         df = pandas.DataFrame.from_records(
             data=self.data.EquipmentUniqueTypes
                 .values('equipment_general_type__name', 'name')
@@ -759,75 +872,68 @@ class Project(object):
 
         return df.loc[df.n_equipment_data_fields == 0] \
             if only0 \
-            else df
+          else df
 
-    def equipment_data_field_names_n_counts(only_multi=True):
+    def _equipment_data_field_names_n_counts(self, only_multi=True):
         d = {(i['equipment_general_type__name'], i['name']): i['n']
-             for i in IoT_DATA_ADMIN_PROJECT.models.base.EquipmentDataField.objects
+             for i in self.data.EquipmentDataFields
                  .values('equipment_general_type__name', 'name')
                  .annotate(n=Count('name'))
                  .order_by('equipment_general_type__name', 'name')}
 
-        return {k: v for k, v in d.items() if v > 1} \
+        return {k: v
+                for k, v in d.items()
+                if v > 1} \
             if only_multi \
-            else d
+          else d
 
-    def equipment_instances_w_cap_urls():
-        _PREFIX = 's3://arimo-panasonic-ap/data/CombinedConfigMeasure/'
-        _PREFIX_LEN = len(_PREFIX)
-
-        ls = []
-
-        for equipment_instance in \
-                IoT_DATA_ADMIN_PROJECT.models.base.EquipmentInstance.objects.filter(data_file_url__startswith=_PREFIX):
-            data_file_url = equipment_instance.data_file_url[_PREFIX_LEN:]
-            if data_file_url != data_file_url.lower():
-                ls.append(data_file_url)
-
-        return ls
-
-    def equipment_instances_n_equipment_unique_types(only_multi=False):
-        s = pandas.DataFrame.from_records(
-            data=IoT_DATA_ADMIN_PROJECT.models.base.EquipmentInstance.objects
+    def _equipment_facilities_n_equipment_instances(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentFacilities
                 .values('name')
-                .annotate(n_equipment_unique_types=Count('equipment_unique_type'))
+                .annotate(n_equipment_instances=Count('equipment_instance'))
                 .order_by('name'),
             index=None,
             exclude=None,
-            columns=['name', 'n_equipment_unique_types'],
+            columns=['name', 'n_equipment_instances'],
             coerce_float=False,
-            nrows=None) \
-            .set_index(
-            keys='name',
-            drop=True,
-            append=False,
-            inplace=False,
-            verify_integrity=True)['n_equipment_unique_types']
+            nrows=None)
 
-        return s.loc[s > 1] \
-            if only_multi \
-            else s
+        return df.loc[df.n_equipment_instances == 0] \
+            if only0 \
+          else df
 
-    def equipment_unique_types_n_equipment_instances_w_data(equipment_general_type_name='refrig'):
-        return pandas.DataFrame.from_records(
-            data=IoT_DATA_ADMIN_PROJECT.models.base.EquipmentInstance.objects
-                .filter(
-                equipment_general_type__name=equipment_general_type_name,
-                data_file_url__startswith='s3://')
-                .values('equipment_unique_type__name')
-                .annotate(n=Count('data_file_url'))
-                .order_by('equipment_unique_type__name'),
+    def _equipment_facilities_n_equipment_systems(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentFacilities
+                .values('name')
+                .annotate(n_equipment_systems=Count('equipment_system'))
+                .order_by('name'),
             index=None,
             exclude=None,
-            columns=['equipment_unique_type__name', 'n'],
+            columns=['name', 'n_equipment_systems'],
             coerce_float=False,
-            nrows=None) \
-            .set_index(
-            keys='equipment_unique_type__name',
-            drop=True,
-            append=False,
-            inplace=False,
-            verify_integrity=True)['n']
+            nrows=None)
+
+        return df.loc[df.n_equipment_systems == 0] \
+            if only0 \
+            else df
+
+    def _equipment_systems_n_equipment_instances(self, only0=True):
+        df = pandas.DataFrame.from_records(
+            data=self.data.EquipmentSystems
+                .values('name')
+                .annotate(n_equipment_instances=Count('equipment_instances'))
+                .order_by('name'),
+            index=None,
+            exclude=None,
+            columns=['name', 'n_equipment_instances'],
+            coerce_float=False,
+            nrows=None)
+
+        return df.loc[df.n_equipment_instances == 0] \
+            if only0 \
+            else df
 
 
 def project(name='TEST'):
