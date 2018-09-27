fc27fda4990a5ef90991bda592bccb6e37579904
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-09-27 12:38:04 -0700

    enhance arimo.IoT.DataAdmin.base.urls

--- arimo/IoT/DataAdmin/base/urls.py
+++ arimo/IoT/DataAdmin/base/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf.urls import url
 
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -16,79 +18,89 @@ from .views import api_root, \
 
 
 API_ROOT_URL = 'api/base'
+DATA_TYPES_API_URL = os.path.join(API_ROOT_URL, 'data-types')
+NUMERIC_MEASUREMENT_UNITS_API_URL = os.path.join(API_ROOT_URL, 'numeric-measurement-units')
+EQUIPMENT_DATA_FIELD_TYPES_API_URL = os.path.join(API_ROOT_URL, 'equipment-data-field-types')
+EQUIPMENT_GENERAL_TYPES_API_URL = os.path.join(API_ROOT_URL, 'equipment-general-types')
+EQUIPMENT_DATA_FIELDS_API_URL = os.path.join(API_ROOT_URL, 'equipment-data-fields')
+EQUIPMENT_UNIQUE_TYPE_GROUPS_API_URL = os.path.join(API_ROOT_URL, 'equipment-unique-type-groups')
+EQUIPMENT_UNIQUE_TYPES_API_URL = os.path.join(API_ROOT_URL, 'equipment-unique-types')
+EQUIPMENT_FACILITIES_API_URL = os.path.join(API_ROOT_URL, 'equipment-facilities')
+EQUIPMENT_INSTANCES_API_URL = os.path.join(API_ROOT_URL, 'equipment-instances')
+EQUIPMENT_SYSTEMS_API_URL = os.path.join(API_ROOT_URL, 'equipment-systems')
 
 
 # API end-points
 urlpatterns = format_suffix_patterns([
     url(r'^{}/$'.format(API_ROOT_URL), api_root),
 
-    url(r'^{}/data-types/$'.format(API_ROOT_URL),
+    url(r'^/$'.format(DATA_TYPES_API_URL),
         DataTypeList.as_view(),
         name='data-type-list'),
-    url(r'^data_types/(?P<pk>[0-9]+)/$',
+    url(r'^/(?P<pk>[0-9]+)/$'.format(DATA_TYPES_API_URL),
         DataTypeDetail.as_view(),
         name='data-type-detail'),
 
-    url(r'^{}/numeric-measurement-units/$'.format(API_ROOT_URL),
+    url(r'^{}/$'.format(NUMERIC_MEASUREMENT_UNITS_API_URL),
         NumericMeasurementUnitList.as_view(),
         name='numeric-measurement-unit-list'),
-    url(r'^numeric_measurement_units/(?P<pk>[0-9]+)/$',
+    url(r'^{}/(?P<pk>[0-9]+)/$'.format(NUMERIC_MEASUREMENT_UNITS_API_URL),
         NumericMeasurementUnitDetail.as_view(),
         name='numeric-measurement-unit-detail'),
 
-    url(r'^{}/equipment-data-field-types/$'.format(API_ROOT_URL),
+    url(r'^{}/$'.format(EQUIPMENT_DATA_FIELD_TYPES_API_URL),
         EquipmentDataFieldTypeList.as_view(),
         name='equipment-data-field-type-list'),
-    url(r'^equipment_data_field_types/(?P<pk>[0-9]+)/$',
+    url(r'^{}/(?P<pk>[0-9]+)/$'.format(EQUIPMENT_DATA_FIELD_TYPES_API_URL),
         EquipmentDataFieldTypeDetail.as_view(),
         name='equipment-data-field-type-detail'),
 
-    url(r'^{}/equipment-general-types/$'.format(API_ROOT_URL),
+    url(r'^{}/$'.format(EQUIPMENT_GENERAL_TYPES_API_URL),
         EquipmentGeneralTypeList.as_view(),
         name='equipment-general-type-list'),
-    url(r'^equipment_general_types/(?P<pk>[0-9]+)/$',
+    url(r'^{}/(?P<pk>[0-9]+)/$'.format(EQUIPMENT_GENERAL_TYPES_API_URL),
         EquipmentGeneralTypeDetail.as_view(),
         name='equipment-general-type-detail'),
 
-    url(r'^{}/equipment-data-fields/$'.format(API_ROOT_URL),
+    url(r'^{}/$'.format(EQUIPMENT_DATA_FIELDS_API_URL),
         EquipmentDataFieldList.as_view(),
         name='equipment-data-field-list'),
-    url(r'^equipment_data_fields/(?P<pk>[0-9]+)/$',
+    url(r'^{}/(?P<pk>[0-9]+)/$'.format(EQUIPMENT_DATA_FIELDS_API_URL),
         EquipmentDataFieldDetail.as_view(),
         name='equipment-data-field-detail'),
 
-    url(r'^{}/equipment-unique-type-groups/$'.format(API_ROOT_URL),
+    url(r'^{}/$'.format(EQUIPMENT_UNIQUE_TYPE_GROUPS_API_URL),
         EquipmentUniqueTypeGroupList.as_view(),
         name='equipment-unique-type-group-list'),
-    url(r'^equipment_unique_type_groups/(?P<pk>[0-9]+)/$',
+    url(r'^{}/(?P<pk>[0-9]+)/$'.format(EQUIPMENT_UNIQUE_TYPE_GROUPS_API_URL),
         EquipmentUniqueTypeGroupDetail.as_view(),
         name='equipment-unique-type-group-detail'),
 
-    url(r'^{}/equipment-unique-types/$'.format(API_ROOT_URL),
+    url(r'^{}/$'.format(EQUIPMENT_UNIQUE_TYPES_API_URL),
         EquipmentUniqueTypeList.as_view(),
         name='equipment-unique-type-list'),
-    url(r'^equipment_unique_types/(?P<pk>[0-9]+)/$',
+    url(r'^{}/(?P<pk>[0-9]+)/$'.format(EQUIPMENT_UNIQUE_TYPES_API_URL),
         EquipmentUniqueTypeDetail.as_view(),
         name='equipment-unique-type-detail'),
 
-    url(r'^{}/equipment_facilities/$'.format(API_ROOT_URL),
+    url(r'^{}/$'.format(EQUIPMENT_FACILITIES_API_URL),
         EquipmentFacilityList.as_view(),
         name='equipment-facility-list'),
-    url(r'^equipment_facilities/(?P<pk>[0-9]+)/$',
+    url(r'^{}/(?P<pk>[0-9]+)/$'.format(EQUIPMENT_FACILITIES_API_URL),
         EquipmentFacilityDetail.as_view(),
         name='equipment-facility-detail'),
 
-    url(r'^{}/equipment_instances/$'.format(API_ROOT_URL),
+    url(r'^{}/$'.format(EQUIPMENT_INSTANCES_API_URL),
         EquipmentInstanceList.as_view(),
         name='equipment-instance-list'),
-    url(r'^equipment_instances/(?P<pk>[0-9]+)/$',
+    url(r'^{}/(?P<pk>[0-9]+)/$'.format(EQUIPMENT_INSTANCES_API_URL),
         EquipmentInstanceDetail.as_view(),
         name='equipment-instance-detail'),
 
-    url(r'^{}/equipment_systems/$'.format(API_ROOT_URL),
+    url(r'^{}/$'.format(EQUIPMENT_SYSTEMS_API_URL),
         EquipmentSystemList.as_view(),
         name='equipment-system-list'),
-    url(r'^equipment_systems/(?P<pk>[0-9]+)/$',
+    url(r'^{}(?P<pk>[0-9]+)/$'.format(EQUIPMENT_SYSTEMS_API_URL),
         EquipmentSystemDetail.as_view(),
         name='equipment-system-detail')
 ])
