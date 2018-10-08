bd80cac10f2df52ffbf5305605f61baa6baa529a
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-10-08 12:26:04 -0700

    update REST code

--- arimo/IoT/DataAdmin/PredMaint/filters.py
+++ arimo/IoT/DataAdmin/PredMaint/filters.py
@@ -3,7 +3,6 @@ from rest_framework_filters import FilterSet, RelatedFilter
 from .models import \
     EquipmentUniqueTypeGroupDataFieldProfile, \
     EquipmentUniqueTypeGroupServiceConfig, \
-    EquipmentUniqueTypeGroupMonitoredDataFieldConfig, \
     Blueprint, \
     EquipmentUniqueTypeGroupDataFieldBlueprintBenchmarkMetricProfile, \
     EquipmentInstanceDailyRiskScore, \
@@ -174,11 +173,158 @@ class BlueprintFilter(FilterSet):
         model = Blueprint
 
         fields = dict(
-            trained_to_date='__all__',
-            timestamp='__all__',
-            uuid='__all__',
-            active='__all__'
-        )
+            trained_to_date=[
+                'exact',   # 'iexact',
+                'gt', 'gte', 'lt', 'lte',
+                'in',
+                'contains',   # 'icontains'
+                'startswith',   # 'istartswith'
+                'endswith',   # 'iendswith',
+                'range',
+                'isnull',
+                # 'regex', 'iregex',
+                'year',   # 'year__iexact'
+                'year__gt', 'year__gte', 'year__lt', 'year__lte',
+                'year__in',
+                # 'year__contains', 'year__icontains',
+                # 'year__startswith', 'year__istartswith', 'year__endswith', year__iendswith',
+                'year__range',
+                # 'year__isnull',
+                # 'year__regex', 'year__iregex',
+                # 'year__contained_by',
+                'month',   # 'month__iexact',
+                'month__gt', 'month__gte', 'month__lt', 'month__lte',
+                'month__in',
+                # 'month__contains', 'month__icontains',
+                # 'month__startswith', 'month__istartswith', 'month__endswith', 'month__iendswith'
+                'month__range',
+                # 'month__isnull',
+                # 'month__regex', 'month__iregex',
+                # 'month__contained_by',
+                # 'day', 'day__iexact',
+                # 'day__gt', 'day__gte', 'day__lt', 'day__lte',
+                # 'day__in',
+                # 'day__contains', 'day__icontains',
+                # 'day__startswith', 'day__istartswith', 'day__endswith', 'day__iendswith',
+                # 'day__range',
+                # 'day__isnull'
+                # 'day__regex', 'day__iregex'
+                # 'day__contained_by'
+                # 'week_day', 'week_day__iexact',
+                # 'week_day__gt', 'week_day__gte', 'week_day__lt', 'week_day__lte',
+                # 'week_day__in',
+                # 'week_day__contains', 'week_day__icontains',
+                # 'week_day__startswith', 'week_day__istartswith', 'week_day__endswith', 'week_day__iendswith',
+                # 'week_day__range',
+                # 'week_day__isnull',
+                # 'week_day__regex', 'week_day__iregex'
+                # 'week_day__contained_by',
+                # 'week', 'week__iexact',
+                # 'week__gt', 'week__gte', 'week__lt', 'week__lte',
+                # 'week__in',
+                # 'week__contains', 'week__icontains',
+                # 'week__startswith', 'week__istartswith', 'week__endswith', 'week__iendswith',
+                # 'week__range',
+                # 'week__isnull',
+                # 'week__regex', 'week__iregex',
+                # 'week__contained_by',
+
+                # django_filters.exceptions.FieldLookupError: Unsupported lookup 'quarter'
+                # 'quarter',   # 'quarter__iexact',
+                # 'quarter__gt', 'quarter__gte', 'quarter__lt', 'quarter__lte',
+                # 'quarter__in',
+                # 'quarter__contains', 'quarter__icontains',
+                # 'quarter__startswith', 'quarter__istartswith', 'quarter__endswith', 'quarter__iendswith',
+                # 'quarter__range'
+                # 'quarter__isnull',
+                # 'quarter__regex', 'quarter__iregex',
+                # 'quarter__contained_by'
+
+                # 'to_date__contained_by'
+            ],
+
+            timestamp=[
+                # 'exact', 'iexact',
+                'gt', 'gte', 'lt', 'lte',
+                # 'in',
+                'contains',   # 'icontains'
+                'startswith',   # 'istartswith'
+                # 'endswith',   'iendswith',
+                'range',
+                # 'isnull',
+                # 'regex', 'iregex',
+                'year',   # 'year__iexact'
+                'year__gt', 'year__gte', 'year__lt', 'year__lte',
+                'year__in',
+                # 'year__contains', 'year__icontains',
+                # 'year__startswith', 'year__istartswith', 'year__endswith', year__iendswith',
+                'year__range',
+                # 'year__isnull',
+                # 'year__regex', 'year__iregex',
+                # 'year__contained_by',
+                'month',   # 'month__iexact',
+                'month__gt', 'month__gte', 'month__lt', 'month__lte',
+                'month__in',
+                # 'month__contains', 'month__icontains',
+                # 'month__startswith', 'month__istartswith', 'month__endswith', 'month__iendswith'
+                'month__range',
+                # 'month__isnull',
+                # 'month__regex', 'month__iregex',
+                # 'month__contained_by',
+                # 'day', 'day__iexact',
+                # 'day__gt', 'day__gte', 'day__lt', 'day__lte',
+                # 'day__in',
+                # 'day__contains', 'day__icontains',
+                # 'day__startswith', 'day__istartswith', 'day__endswith', 'day__iendswith',
+                # 'day__range',
+                # 'day__isnull'
+                # 'day__regex', 'day__iregex'
+                # 'day__contained_by'
+                # 'week_day', 'week_day__iexact',
+                # 'week_day__gt', 'week_day__gte', 'week_day__lt', 'week_day__lte',
+                # 'week_day__in',
+                # 'week_day__contains', 'week_day__icontains',
+                # 'week_day__startswith', 'week_day__istartswith', 'week_day__endswith', 'week_day__iendswith',
+                # 'week_day__range',
+                # 'week_day__isnull',
+                # 'week_day__regex', 'week_day__iregex'
+                # 'week_day__contained_by',
+                # 'week', 'week__iexact',
+                # 'week__gt', 'week__gte', 'week__lt', 'week__lte',
+                # 'week__in',
+                # 'week__contains', 'week__icontains',
+                # 'week__startswith', 'week__istartswith', 'week__endswith', 'week__iendswith',
+                # 'week__range',
+                # 'week__isnull',
+                # 'week__regex', 'week__iregex',
+                # 'week__contained_by',
+
+                # django_filters.exceptions.FieldLookupError: Unsupported lookup 'quarter'
+                # 'quarter',   # 'quarter__iexact',
+                # 'quarter__gt', 'quarter__gte', 'quarter__lt', 'quarter__lte',
+                # 'quarter__in',
+                # 'quarter__contains', 'quarter__icontains',
+                # 'quarter__startswith', 'quarter__istartswith', 'quarter__endswith', 'quarter__iendswith',
+                # 'quarter__range'
+                # 'quarter__isnull',
+                # 'quarter__regex', 'quarter__iregex',
+                # 'quarter__contained_by'
+
+                # 'contained_by'
+                ],
+
+            uuid=[
+                'exact', 'iexact',
+                # 'gt', 'gte', 'lt', 'lte',
+                'in',
+                'contains', 'icontains',
+                'startswith', 'istartswith', 'endswith', 'iendswith',
+                # 'range',
+                # 'isnull',
+                # 'regex', 'iregex'
+            ],
+
+            active=['exact'])
 
 
 class EquipmentUniqueTypeGroupDataFieldBlueprintBenchmarkMetricProfileFilter(FilterSet):
