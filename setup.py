b61440d7ff7e4c2f7aaef5a48aa703835ab39ca2
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-10-25 10:27:18 -0700

    minor fixes to setup.py

--- setup.py
+++ setup.py
@@ -27,13 +27,13 @@ for s in open(_REQUIREMENTS_FILE_NAME).readlines():
         lower_s = s.lower()
         install_requires.append(
             'Django'
-            if lower_s.startswith('django >=')
+            if lower_s.startswith('django')
             else ('Django-AutoComplete-Light == 3.2.10'
-                  if six.PY2 and lower_s.startswith('django-autocomplete-light >=')
+                  if six.PY2 and lower_s.startswith('django-autocomplete-light')
                   else ('DjangoRESTFramework-Filters == 0.10.2'
-                        if six.PY2 and lower_s.startswith('djangorestframework-filters >=')
+                        if six.PY2 and lower_s.startswith('djangorestframework-filters')
                         else ('Django-Filter == 1.1.0'
-                              if six.PY2 and lower_s.startswith('django-filter >=')
+                              if six.PY2 and lower_s.startswith('django-filter')
                               else s))))
 
 setup(
