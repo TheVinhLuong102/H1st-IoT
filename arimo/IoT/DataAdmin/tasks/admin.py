93243df15f621edf724dc42a4a77054aea8782e1
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-12-05 10:36:12 -0800

    add arimo.IoT.DataAdmin.tasks.models.EquipmentUniqueTypeGroupCopyRiskScoresToDBTask

--- arimo/IoT/DataAdmin/tasks/admin.py
+++ arimo/IoT/DataAdmin/tasks/admin.py
@@ -3,7 +3,7 @@ from django.contrib.admin import ModelAdmin, site
 from silk.profiling.profiler import silk_profile
 
 from .models import \
-    EquipmentUniqueTypeGroupRiskScoringTask
+    EquipmentUniqueTypeGroupRiskScoringTask, EquipmentUniqueTypeGroupCopyRiskScoresToDBTask
 
 
 class EquipmentUniqueTypeGroupRiskScoringTaskAdmin(ModelAdmin):
@@ -49,3 +49,48 @@ class EquipmentUniqueTypeGroupRiskScoringTaskAdmin(ModelAdmin):
 site.register(
     EquipmentUniqueTypeGroupRiskScoringTask,
     admin_class=EquipmentUniqueTypeGroupRiskScoringTaskAdmin)
+
+
+class EquipmentUniqueTypeGroupCopyRiskScoresToDBTaskAdmin(ModelAdmin):
+    list_display = \
+        'equipment_unique_type_group', \
+        'date', \
+        'finished'
+
+    list_filter = \
+        'equipment_unique_type_group__equipment_general_type__name', \
+        'equipment_unique_type_group__name', \
+        'date'
+
+    search_fields = \
+        'equipment_unique_type_group__equipment_general_type__name', \
+        'equipment_unique_type_group__name'
+
+    list_select_related = \
+        'equipment_unique_type_group', 'equipment_unique_type_group__equipment_general_type'
+
+    show_full_result_count = False   # too many
+
+    readonly_fields = \
+        'equipment_unique_type_group', \
+        'date', \
+        'finished'
+
+    @silk_profile(name='ADMIN: Equipment Unique Type Group Copy Risk Scores to DB Tasks')
+    def changelist_view(self, request, extra_context=None):
+        return super(EquipmentUniqueTypeGroupCopyRiskScoresToDBTaskAdmin, self).changelist_view(
+                request=request,
+                extra_context=extra_context)
+
+    @silk_profile(name='ADMIN: Equipment Unique Type Group Copy Risk Scores to DB  Task')
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        return super(EquipmentUniqueTypeGroupCopyRiskScoresToDBTaskAdmin, self).changeform_view(
+                request=request,
+                object_id=object_id,
+                form_url=form_url,
+                extra_context=extra_context)
+
+
+site.register(
+    EquipmentUniqueTypeGroupCopyRiskScoresToDBTask,
+    admin_class=EquipmentUniqueTypeGroupCopyRiskScoresToDBTaskAdmin)
--- arimo/IoT/DataAdmin/tasks/models.py
+++ arimo/IoT/DataAdmin/tasks/models.py
@@ -42,3 +42,38 @@ class EquipmentUniqueTypeGroupRiskScoringTask(Model):
         ordering = \
             'equipment_unique_type_group', \
             '-date'
+
+
+class EquipmentUniqueTypeGroupCopyRiskScoresToDBTask(Model):
+    RELATED_NAME = 'equipment_unique_type_group_copy_risk_scores_to_db_task'
+    RELATED_QUERY_NAME = 'equipment_unique_type_group_copy_risk_scores_to_db_tasks'
+
+    equipment_unique_type_group = \
+        ForeignKey(
+            to=EquipmentUniqueTypeGroup,
+            related_name=RELATED_NAME,
+            related_query_name=RELATED_QUERY_NAME,
+            blank=False,
+            null=False,
+            on_delete=PROTECT)
+
+    date = \
+        DateField(
+            blank=False,
+            null=False,
+            default=None,
+            auto_now=False,
+            auto_now_add=False)
+
+    finished = \
+        DateTimeField(
+            blank=True,
+            null=True,
+            default=None,
+            auto_now=False,
+            auto_now_add=False)
+
+    class Meta:
+        ordering = \
+            'equipment_unique_type_group', \
+            '-date'
