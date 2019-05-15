d6208d15324a734e0a962f441770958d9723b628
Vinh Luong <MBALearnsToCode@UChicago.edu>
2019-05-14 19:07:47 -0700

    Revert "try debugging arimo.IoT.DataAdmin.base.admin.EquipmentInstanceDailyMetadataAdmin"
    This reverts commit a694f9648514f3aa1cadaf32c575f62437a690f8.

--- arimo/IoT/DataAdmin/base/admin.py
+++ arimo/IoT/DataAdmin/base/admin.py
@@ -513,8 +513,7 @@ class EquipmentInstanceDailyMetadataAdmin(ModelAdmin):
     list_select_related = \
         'equipment_instance', \
         'equipment_instance__equipment_general_type', \
-        'equipment_instance__equipment_unique_type', \
-        'equipment_instance__equipment_unique_type__equipment_general_type'
+        'equipment_instance__equipment_unique_type'
 
     readonly_fields = \
         'equipment_instance', \
