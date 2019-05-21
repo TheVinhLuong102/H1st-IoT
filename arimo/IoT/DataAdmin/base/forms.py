3046b5622169720c37b70ce10b33b331d688f3cf
Vinh Luong <MBALearnsToCode@UChicago.edu>
2019-05-21 15:12:48 -0700

    refactor arimo.IoT.DataAdmin.base.forms

--- arimo/IoT/DataAdmin/base/forms.py
+++ arimo/IoT/DataAdmin/base/forms.py
@@ -16,41 +16,61 @@ from .models import \
     EquipmentDataField, \
     EquipmentUniqueTypeGroup, \
     EquipmentUniqueType, \
+    EquipmentFacility, \
     EquipmentInstance, \
     EquipmentSystem
 
 
-class EquipmentComponentForm(autocomplete.FutureModelForm):
-    equipment_data_fields = \
-        ModelMultipleChoiceField(
-            queryset=
-                EquipmentDataField.objects
-                .defer('description', 'last_updated')
-                .select_related(
-                    'equipment_general_type',
-                    'equipment_data_field_type',
-                    'data_type',
-                    'numeric_measurement_unit')
-                .defer(
-                    'numeric_measurement_unit__description'),
-            widget=
-                autocomplete.ModelSelect2Multiple(
-                    url=EquipmentDataFieldAutoComplete.name,
-                    attrs={'data-minimum-input-length': 1}),
-            required=False)
+EQUIPMENT_COMPONENTS_MODEL_MULTIPLE_CHOICE_FIELD = \
+    ModelMultipleChoiceField(
+        queryset=
+            EquipmentComponent.objects
+            .defer('description', 'last_updated')
+            .select_related(
+                'equipment_general_type'),
+        widget=
+            autocomplete.ModelSelect2Multiple(
+                url=EquipmentComponentAutoComplete.name),
+        required=False)
 
-    equipment_unique_types = \
-        ModelMultipleChoiceField(
-            queryset=
-                EquipmentUniqueType.objects
-                .defer('description', 'last_updated')
-                .select_related(
-                    'equipment_general_type'),
-            widget=
-                autocomplete.ModelSelect2Multiple(
-                    url=EquipmentUniqueTypeAutoComplete.name,
-                    attrs={'data-minimum-input-length': 1}),
-            required=False)
+
+EQUIPMENT_DATA_FIELDS_MODEL_MULTIPLE_CHOICE_FIELD = \
+    ModelMultipleChoiceField(
+        queryset=
+            EquipmentDataField.objects
+            .defer('description', 'last_updated')
+            .select_related(
+                'equipment_general_type',
+                'equipment_data_field_type',
+                'data_type',
+                'numeric_measurement_unit')
+            .defer(
+                'numeric_measurement_unit__description'),
+        widget=
+            autocomplete.ModelSelect2Multiple(
+                url=EquipmentDataFieldAutoComplete.name,
+                attrs={'data-minimum-input-length': 1}),
+        required=False)
+
+
+EQUIPMENT_UNIQUE_TYPES_MODEL_MULTIPLE_CHOICE_FIELD = \
+    ModelMultipleChoiceField(
+        queryset=
+            EquipmentUniqueType.objects
+            .defer('description', 'last_updated')
+            .select_related(
+                'equipment_general_type'),
+        widget=
+            autocomplete.ModelSelect2Multiple(
+                url=EquipmentUniqueTypeAutoComplete.name,
+                attrs={'data-minimum-input-length': 1}),
+        required=False)
+
+
+class EquipmentComponentForm(autocomplete.FutureModelForm):
+    equipment_data_fields = EQUIPMENT_DATA_FIELDS_MODEL_MULTIPLE_CHOICE_FIELD
+
+    equipment_unique_types = EQUIPMENT_UNIQUE_TYPES_MODEL_MULTIPLE_CHOICE_FIELD
 
     class Meta:
         model = EquipmentComponent
@@ -59,30 +79,9 @@ class EquipmentComponentForm(autocomplete.FutureModelForm):
 
 
 class EquipmentDataFieldForm(autocomplete.FutureModelForm):
-    equipment_components = \
-        ModelMultipleChoiceField(
-            queryset=
-                EquipmentComponent.objects
-                .defer('description', 'last_updated')
-                .select_related(
-                    'equipment_general_type'),
-            widget=
-                autocomplete.ModelSelect2Multiple(
-                    url=EquipmentComponentAutoComplete.name),
-            required=False)
+    equipment_components = EQUIPMENT_COMPONENTS_MODEL_MULTIPLE_CHOICE_FIELD
 
-    equipment_unique_types = \
-        ModelMultipleChoiceField(
-            queryset=
-                EquipmentUniqueType.objects
-                .defer('description', 'last_updated')
-                .select_related(
-                    'equipment_general_type'),
-            widget=
-                autocomplete.ModelSelect2Multiple(
-                    url=EquipmentUniqueTypeAutoComplete.name,
-                    attrs={'data-minimum-input-length': 1}),
-            required=False)
+    equipment_unique_types = EQUIPMENT_UNIQUE_TYPES_MODEL_MULTIPLE_CHOICE_FIELD
 
     class Meta:
         model = EquipmentDataField
@@ -91,18 +90,7 @@ class EquipmentDataFieldForm(autocomplete.FutureModelForm):
 
 
 class EquipmentUniqueTypeGroupForm(autocomplete.FutureModelForm):
-    equipment_unique_types = \
-        ModelMultipleChoiceField(
-            queryset=
-                EquipmentUniqueType.objects
-                .defer('description', 'last_updated')
-                .select_related(
-                    'equipment_general_type'),
-            widget=
-                autocomplete.ModelSelect2Multiple(
-                    url=EquipmentUniqueTypeAutoComplete.name,
-                    attrs={'data-minimum-input-length': 1}),
-            required=False)
+    equipment_unique_types = EQUIPMENT_UNIQUE_TYPES_MODEL_MULTIPLE_CHOICE_FIELD
 
     class Meta:
         model = EquipmentUniqueTypeGroup
@@ -111,35 +99,9 @@ class EquipmentUniqueTypeGroupForm(autocomplete.FutureModelForm):
 
 
 class EquipmentUniqueTypeForm(autocomplete.FutureModelForm):
-    equipment_components = \
-        ModelMultipleChoiceField(
-            queryset=
-                EquipmentComponent.objects
-                .defer('description', 'last_updated')
-                .select_related(
-                    'equipment_general_type'),
-            widget=
-                autocomplete.ModelSelect2Multiple(
-                    url=EquipmentComponentAutoComplete.name),
-            required=False)
+    equipment_components = EQUIPMENT_COMPONENTS_MODEL_MULTIPLE_CHOICE_FIELD
 
-    equipment_data_fields = \
-        ModelMultipleChoiceField(
-            queryset=
-                EquipmentDataField.objects
-                .defer('description', 'last_updated')
-                .select_related(
-                    'equipment_general_type',
-                    'equipment_data_field_type',
-                    'data_type',
-                    'numeric_measurement_unit')
-                .defer(
-                    'numeric_measurement_unit__description'),
-            widget=
-                autocomplete.ModelSelect2Multiple(
-                    url=EquipmentDataFieldAutoComplete.name,
-                    attrs={'data-minimum-input-length': 1}),
-            required=False)
+    equipment_data_fields = EQUIPMENT_DATA_FIELDS_MODEL_MULTIPLE_CHOICE_FIELD
 
     equipment_unique_type_groups = \
         ModelMultipleChoiceField(
@@ -165,12 +127,21 @@ class EquipmentInstanceForm(autocomplete.FutureModelForm):
             queryset=
                 EquipmentUniqueType.objects
                 .select_related(
-                    'equipment_general_type'),
+                    'equipment_general_type')
+                .order_by(),
             widget=
                 autocomplete.ModelSelect2(
                     url=EquipmentUniqueTypeAutoComplete.name,
                     attrs={'data-minimum-input-length': 1}),
             required=False)
+
+    equipment_facility = \
+        ModelChoiceField(
+            queryset=EquipmentFacility.objects.only('name').order_by(),
+            widget=
+                autocomplete.ModelSelect2(
+                    url=EquipmentFacilityAutoComplete.name,
+                    attrs={'data-minimum-input-length': 1}))
 
     class Meta:
         model = EquipmentInstance
@@ -180,12 +151,7 @@ class EquipmentInstanceForm(autocomplete.FutureModelForm):
         widgets = dict(
             equipment_general_type=
                 autocomplete.ModelSelect2(
-                    url=EquipmentGeneralTypeAutoComplete.name),
-
-            equipment_facility=
-                autocomplete.ModelSelect2(
-                    url=EquipmentFacilityAutoComplete.name,
-                    attrs={'data-minimum-input-length': 1}))
+                    url=EquipmentGeneralTypeAutoComplete.name))
 
 
 class EquipmentSystemForm(autocomplete.FutureModelForm):
