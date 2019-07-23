48db5ab0738ebac97b25518bcfd01f48510c322d
Vinh Luong <MBALearnsToCode@UChicago.edu>
2019-07-23 16:47:58 -0700

    minor refactoring to arimo.IoT.DataAdmin.Project.load_equipment_data(...)

--- arimo/IoT/DataAdmin/__init__.py
+++ arimo/IoT/DataAdmin/__init__.py
@@ -203,29 +203,29 @@ class Project(object):
                 verbose=verbose, **kwargs)
                if _spark
                else S3ParquetDataFeeder(
-                path=path,
-                aws_access_key_id=self.params.s3.access_key_id,
-                aws_secret_access_key=self.params.s3.secret_access_key,
-                iCol=self._EQUIPMENT_INSTANCE_ID_COL_NAME
-                    if set_i_col
-                    else None,
-                tCol=self._DATE_TIME_COL_NAME
-                    if set_t_col
-                    else None,
-                verbose=verbose, **kwargs)) \
+                        path=path,
+                        aws_access_key_id=self.params.s3.access_key_id,
+                        aws_secret_access_key=self.params.s3.secret_access_key,
+                        iCol=self._EQUIPMENT_INSTANCE_ID_COL_NAME
+                            if set_i_col
+                            else None,
+                        tCol=self._DATE_TIME_COL_NAME
+                            if set_t_col
+                            else None,
+                        verbose=verbose, **kwargs)) \
             if _from_files \
             else DDF.load(
-                path=path,
-                format='parquet', mergeSchema=True,
-                aws_access_key_id=self.params.s3.access_key_id,
-                aws_secret_access_key=self.params.s3.secret_access_key,
-                iCol=self._EQUIPMENT_INSTANCE_ID_COL_NAME
-                    if set_i_col
-                    else None,
-                tCol=self._DATE_TIME_COL_NAME
-                    if set_t_col
-                    else None,
-                verbose=verbose, **kwargs)
+                    path=path,
+                    format='parquet', mergeSchema=True,
+                    aws_access_key_id=self.params.s3.access_key_id,
+                    aws_secret_access_key=self.params.s3.secret_access_key,
+                    iCol=self._EQUIPMENT_INSTANCE_ID_COL_NAME
+                        if set_i_col
+                        else None,
+                    tCol=self._DATE_TIME_COL_NAME
+                        if set_t_col
+                        else None,
+                    verbose=verbose, **kwargs)
 
         assert {self._EQUIPMENT_INSTANCE_ID_COL_NAME, DATE_COL, self._DATE_TIME_COL_NAME}.issubset(df.columns)
 
