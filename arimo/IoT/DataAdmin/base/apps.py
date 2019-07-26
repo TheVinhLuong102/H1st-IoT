29ec1f3b43454e810b60f440ffec3f049f859fea
Vinh Luong <MBALearnsToCode@UChicago.edu>
2019-07-26 11:48:29 -0700

    edit app labels

--- arimo/IoT/DataAdmin/base/apps.py
+++ arimo/IoT/DataAdmin/base/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class BaseConfig(AppConfig):
     name = 'arimo.IoT.DataAdmin.base'
     label = 'Arimo_IoT_DataAdmin_Base'
-    verbose_name = 'Arimo IoT Data Admin'
+    verbose_name = 'Data Admin'
--- arimo/IoT/DataAdmin/tasks/apps.py
+++ arimo/IoT/DataAdmin/tasks/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class TasksConfig(AppConfig):
     name = 'arimo.IoT.DataAdmin.tasks'
     label = 'Arimo_IoT_DataAdmin_Tasks'
-    verbose_name = 'Arimo IoT Data Admin Tasks'
+    verbose_name = 'Tasks'
