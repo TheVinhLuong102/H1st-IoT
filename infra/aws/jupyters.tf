e72669b7e388745e0213a49034162224f99ed48c
An Phan <phanhongan@gmail.com>
2019-07-29 16:23:35 +0700

    Adđ jupyter-gamma gpu cluster; Upgrade all gpu clusters to g3.16xlarge

--- infra/aws/jupyters.tf
+++ infra/aws/jupyters.tf
@@ -7,7 +7,7 @@ module "jupyter-alpha" {
   instance_profile   = aws_iam_instance_profile.yarn.name
   ssh_user_data      = module.ssh.user_data
 
-  instance_type = "g3.8xlarge"
+  instance_type = "g3.16xlarge"
 
   domain = "jupyter-alpha.jp"
 }
@@ -21,8 +21,21 @@ module "jupyter-beta" {
   instance_profile   = aws_iam_instance_profile.yarn.name
   ssh_user_data      = module.ssh.user_data
 
-  instance_type = "g3.8xlarge"
+  instance_type = "g3.16xlarge"
 
   domain = "jupyter-beta.jp"
 }
 
+module "jupyter-gamma" {
+  source = "./gpu-jupyter"
+
+  name               = "jupyter-gamma"
+  subnet_id          = var.subnet1_id
+  security_group_ids = [var.internal_security_group_id]
+  instance_profile   = aws_iam_instance_profile.yarn.name
+  ssh_user_data      = module.ssh.user_data
+
+  instance_type = "g3.16xlarge"
+
+  domain = "jupyter-gamma.jp"
+}
