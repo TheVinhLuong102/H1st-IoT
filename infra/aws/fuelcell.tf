553a5d26194b9252c8ac2bb45acc30c17137c6c3
Bao Nguyen <baonguyen@arimo.com>
2019-10-02 09:40:39 -0700

    Add fuelcell

--- infra/aws/fuelcell.tf
+++ infra/aws/fuelcell.tf
@@ -1,3 +1,48 @@
+data "aws_iam_policy_document" "fc_s3_access" {
+  statement {
+    actions = ["s3:*"]
+    resources = [
+      "arn:aws:s3:::arimo-bai/*",
+      "arn:aws:s3:::arimo-iot-pm/*",
+      # "arn:aws:s3:::arimo-panasonic-ap",
+      # "arn:aws:s3:::arimo-panasonic-ap/*",
+      "arn:aws:s3:::arimo-panasonic-ap-jp-cc-pm",
+      "arn:aws:s3:::arimo-panasonic-ap-jp-cc-pm/*",
+      "arn:aws:s3:::arimo-panasonic-ap-jp-fc-pm",
+      "arn:aws:s3:::arimo-panasonic-ap-jp-fc-pm/*",
+      "arn:aws:s3:::arimo-panasonic-iot-pm",
+      "arn:aws:s3:::arimo-panasonic-iot-pm/*",
+      "arn:aws:s3:::arimo-bai-clusters/custom_provisioner/ccpm*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:ListAllMyBuckets",
+    ]
+
+    resources = ["arn:aws:s3:::*"]
+  }
+}
+
+resource "aws_iam_role" "fc_role" {
+  name               = "fcpm-yarn"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_instance_profile" "fc_role" {
+  name = aws_iam_role.fc_role.name
+  role = aws_iam_role.fc_role.name
+}
+
+resource "aws_iam_role_policy" "fc_node_s3_policy" {
+  name   = "fc-s3-access"
+  policy = data.aws_iam_policy_document.fc_s3_access.json
+  role   = aws_iam_role.fc_role.id
+}
+
+
 resource "aws_db_instance" "fcpm-app-db" {
   allocated_storage       = 864
   backup_retention_period = 3
@@ -67,4 +112,45 @@ module "fc-app-ingress" {
   domain_prefix      = "fc-app"
   instances          = [aws_instance.fc-app.id]
   instance_count     = 1
+}
+
+module "fc-jupyter" {
+  source = "./gpu-jupyter"
+
+  name               = "fcpm-jupyter"
+  subnet_id          = var.subnet1_id
+  security_group_ids = [var.internal_security_group_id]
+  instance_profile   = aws_iam_instance_profile.fc_role.name
+  ssh_user_data      = module.ssh.user_data
+
+  project_tag = "FCPM"
+
+  instance_type = "g3.16xlarge"
+
+  domain = "fcpm-jupyter.jp"
+}
+
+module "fc-yarn-xlarge" {
+  source             = "./yarn-cluster"
+  name               = "fcpm-xlarge"
+  domain             = "yarn-fcpm-xlarge.jp"
+  subnet_id          = var.subnet1_id
+  security_group_ids = [var.internal_security_group_id]
+  instance_profile   = aws_iam_instance_profile.fc_role.name
+
+  installer_version  = "1.3.2"
+
+  ssh_user_data    = module.ssh.user_data
+  s3_config_bucket = var.storage_bucket_name
+
+  # TODO : support ebs for master node
+
+  workers              = 3
+  instance_type        = "c5n.18xlarge"
+  master_instance_type = "m5a.2xlarge"
+  yarn_worker_memory   = "192000"
+  project_tag          = "FCPM"
+  environment_tag      = var.environment_tag
+
+  cluster_version      = "20190802"
 }
