6aec5cc662d36acfd0e4dcfd40ca81a9ef813fae
Bao Nguyen <baonguyen@arimo.com>
2019-10-03 15:48:51 -0700

    update tf 2.0

--- infra/aws/gpu-jupyter/templates/install.sh
+++ infra/aws/gpu-jupyter/templates/install.sh
@@ -20,6 +20,7 @@ ldconfig -v
 
 # this is so weird ???
 export PATH=/usr/local/cuda:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+tf_version=2.0.0-gpu-py3-jupyter
 
 docker run --name jupyter -d -it --restart always --privileged \
     -u $(id -u):$(id -g) \
@@ -28,4 +29,4 @@ docker run --name jupyter -d -it --restart always --privileged \
     -e PS1="\\W \\$ " \
     -v /opt/arimo/data:/tf \
     -p 80:8888 $runtime \
-    tensorflow/tensorflow:1.13.1-gpu-jupyter
+    tensorflow/tensorflow:$tf_version
