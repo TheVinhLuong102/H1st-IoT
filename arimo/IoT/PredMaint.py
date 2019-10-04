dfba07d039b2bb4fc15547abb49405277f3e8949
Vinh Luong <MBALearnsToCode@UChicago.edu>
2019-10-04 14:38:52 -0700

    refactor arimo.IoT.PredMaint.Project

--- arimo/IoT/PredMaint.py
+++ arimo/IoT/PredMaint.py
@@ -361,7 +361,7 @@ class Project(object):
 
     def load_equipment_data(
             self, equipment_data_set_name,
-            spark=False, set_i_col=True, set_t_col=True,
+            spark=False, set_i_col=False, set_t_col=False,
             verbose=True, **kwargs):
         path = os.path.join(
                 self.params.s3.equipment_data.dir_path,
@@ -427,17 +427,17 @@ class Project(object):
                         equipment_general_type_name.upper(),
                         equipment_unique_type_group_name)
 
-                s3_parquet_df = \
+                equipment_unique_type_group_s3_parquet_df = \
                     self.load_equipment_data(
                         equipment_unique_type_group_data_set_name,
-                        spark=False, set_i_col=True, set_t_col=True,
+                        spark=False, set_i_col=True, set_t_col=False,
                         verbose=True)
 
                 if to_month:
                     to_date = month_end(to_month)
 
-                    s3_parquet_df = \
-                        s3_parquet_df.filterByPartitionKeys(
+                    equipment_unique_type_group_s3_parquet_df = \
+                        equipment_unique_type_group_s3_parquet_df.filterByPartitionKeys(
                             (DATE_COL,
                              str((datetime.datetime.strptime('{}-01'.format(to_month), '%Y-%m-%d') -
                                   relativedelta(months=self.REF_N_MONTHS - 1)).date()),
@@ -459,13 +459,14 @@ class Project(object):
                 for equipment_data_field in tqdm.tqdm(equipment_unique_type_group.equipment_data_fields.all()):
                     equipment_data_field_name = equipment_data_field.name
 
-                    if equipment_data_field_name in s3_parquet_df.contentCols:
-                        if s3_parquet_df.typeIsNum(equipment_data_field_name):
-                            s3_parquet_df._nulls[equipment_data_field_name] = \
+                    if equipment_data_field_name in equipment_unique_type_group_s3_parquet_df.possibleFeatureContentCols:
+                        if equipment_unique_type_group_s3_parquet_df.typeIsNum(equipment_data_field_name):
+                            equipment_unique_type_group_s3_parquet_df._nulls[equipment_data_field_name] = \
                                 equipment_data_field.lower_numeric_null, \
                                 equipment_data_field.upper_numeric_null
 
-                        _distinct_values_proportions = s3_parquet_df.distinct(equipment_data_field_name).to_dict()
+                        _distinct_values_proportions = \
+                            equipment_unique_type_group_s3_parquet_df.distinct(equipment_data_field_name).to_dict()
                         _n_distinct_values = len(_distinct_values_proportions)
 
                         equipment_unique_type_group_data_field_profile = \
@@ -473,15 +474,16 @@ class Project(object):
                                 equipment_unique_type_group=equipment_unique_type_group,
                                 equipment_data_field=equipment_data_field,
                                 to_date=to_date,
-                                valid_proportion=s3_parquet_df.nonNullProportion(equipment_data_field_name),
+                                valid_proportion=
+                                    equipment_unique_type_group_s3_parquet_df.nonNullProportion(equipment_data_field_name),
                                 n_distinct_values=_n_distinct_values)
 
                         if _n_distinct_values <= self._MAX_N_DISTINCT_VALUES_TO_PROFILE:
                             equipment_unique_type_group_data_field_profile.distinct_values = _distinct_values_proportions
 
-                        if s3_parquet_df.typeIsNum(equipment_data_field_name):
+                        if equipment_unique_type_group_s3_parquet_df.typeIsNum(equipment_data_field_name):
                             quartiles = \
-                                s3_parquet_df.reprSample[equipment_data_field_name] \
+                                equipment_unique_type_group_s3_parquet_df.reprSample[equipment_data_field_name] \
                                 .describe(
                                     percentiles=(.25, .5, .75)) \
                                 .drop(
@@ -493,12 +495,12 @@ class Project(object):
 
                             equipment_unique_type_group_data_field_profile.sample_min = quartiles['min']
                             equipment_unique_type_group_data_field_profile.outlier_rst_min = \
-                                s3_parquet_df.outlierRstMin(equipment_data_field_name)
+                                equipment_unique_type_group_s3_parquet_df.outlierRstMin(equipment_data_field_name)
                             equipment_unique_type_group_data_field_profile.sample_quartile = quartiles['25%']
                             equipment_unique_type_group_data_field_profile.sample_median = quartiles['50%']
                             equipment_unique_type_group_data_field_profile.sample_3rd_quartile = quartiles['75%']
                             equipment_unique_type_group_data_field_profile.outlier_rst_max = \
-                                s3_parquet_df.outlierRstMax(equipment_data_field_name)
+                                equipment_unique_type_group_s3_parquet_df.outlierRstMax(equipment_data_field_name)
                             equipment_unique_type_group_data_field_profile.sample_max = quartiles['max']
 
                         equipment_unique_type_group_data_field_profile.save()
@@ -535,10 +537,10 @@ class Project(object):
                         equipment_general_type_name.upper(),
                         equipment_unique_type_group_name)
 
-                s3_parquet_df = \
+                equipment_unique_type_group_s3_parquet_df = \
                     self.load_equipment_data(
                         equipment_unique_type_group_data_set_name,
-                        spark=False, set_i_col=True, set_t_col=True,
+                        spark=False, set_i_col=True, set_t_col=False,
                         verbose=True)
 
                 equipment_unique_type_group = \
@@ -560,13 +562,13 @@ class Project(object):
                     equipment_data_field = equipment_data_fields[i]
                     equipment_data_field_name = equipment_data_field.name
 
-                    if equipment_data_field_name in s3_parquet_df.possibleNumContentCols:
-                        s3_parquet_df._nulls[equipment_data_field_name] = \
+                    if equipment_data_field_name in equipment_unique_type_group_s3_parquet_df.possibleNumContentCols:
+                        equipment_unique_type_group_s3_parquet_df._nulls[equipment_data_field_name] = \
                             equipment_data_field.lower_numeric_null, \
                             equipment_data_field.upper_numeric_null
 
-                        outlier_rst_min = s3_parquet_df.outlierRstMin(equipment_data_field_name)
-                        outlier_rst_max = s3_parquet_df.outlierRstMax(equipment_data_field_name)
+                        outlier_rst_min = equipment_unique_type_group_s3_parquet_df.outlierRstMin(equipment_data_field_name)
+                        outlier_rst_max = equipment_unique_type_group_s3_parquet_df.outlierRstMax(equipment_data_field_name)
 
                         if outlier_rst_min < outlier_rst_max:
                             for i_2 in tqdm.tqdm(range(i + 1, n_equipment_data_fields)):
@@ -574,22 +576,22 @@ class Project(object):
                                 equipment_data_field_name_2 = equipment_data_field_2.name
 
                                 if (equipment_data_field_name_2 != equipment_data_field_name) and \
-                                        (equipment_data_field_name_2 in s3_parquet_df.possibleNumContentCols):
-                                    s3_parquet_df._nulls[equipment_data_field_name_2] = \
+                                        (equipment_data_field_name_2 in equipment_unique_type_group_s3_parquet_df.possibleNumContentCols):
+                                    equipment_unique_type_group_s3_parquet_df._nulls[equipment_data_field_name_2] = \
                                         equipment_data_field_2.lower_numeric_null, \
                                         equipment_data_field_2.upper_numeric_null
 
-                                    outlier_rst_min_2 = s3_parquet_df.outlierRstMin(equipment_data_field_name_2)
-                                    outlier_rst_max_2 = s3_parquet_df.outlierRstMax(equipment_data_field_name_2)
+                                    outlier_rst_min_2 = equipment_unique_type_group_s3_parquet_df.outlierRstMin(equipment_data_field_name_2)
+                                    outlier_rst_max_2 = equipment_unique_type_group_s3_parquet_df.outlierRstMax(equipment_data_field_name_2)
 
                                     if outlier_rst_min_2 < outlier_rst_max_2:
                                         sample_df = \
-                                            s3_parquet_df.reprSample.loc[
-                                                s3_parquet_df.reprSample[equipment_data_field_name].between(
+                                            equipment_unique_type_group_s3_parquet_df.reprSample.loc[
+                                                equipment_unique_type_group_s3_parquet_df.reprSample[equipment_data_field_name].between(
                                                     outlier_rst_min,
                                                     outlier_rst_max,
                                                     inclusive=False) &
-                                                s3_parquet_df.reprSample[equipment_data_field_name_2].between(
+                                                equipment_unique_type_group_s3_parquet_df.reprSample[equipment_data_field_name_2].between(
                                                     outlier_rst_min_2,
                                                     outlier_rst_max_2,
                                                     inclusive=False),
@@ -1167,7 +1169,7 @@ class Project(object):
                             s3_parquet_ddf = \
                                 self.load_equipment_data(
                                     equipment_unique_type_group_data_set_name,
-                                    spark=True, set_i_col=False) \
+                                    spark=True, set_i_col=False, set_t_col=True) \
                                 .filterByPartitionKeys(
                                     (DATE_COL,
                                      _mth_str + '-01',
@@ -1183,7 +1185,7 @@ class Project(object):
                     equipment_unique_type_group_s3_parquet_ddf = \
                         self.load_equipment_data(
                             equipment_unique_type_group_data_set_name,
-                            spark=True, set_i_col=False)
+                            spark=True, set_i_col=False, set_t_col=True)
 
                     try:
                         s3_parquet_ddf = \
@@ -1290,7 +1292,7 @@ class Project(object):
                     equipment_unique_type_group_s3_parquet_ddf = \
                         self.load_equipment_data(
                             equipment_unique_type_group_data_set_name,
-                            spark=True, set_i_col=False)
+                            spark=True, set_i_col=False, set_t_col=True)
 
                     try:
                         # load to make sure the equipment data does exist properly
@@ -1328,7 +1330,7 @@ class Project(object):
                     s3_parquet_ddf = \
                         self.load_equipment_data(
                             equipment_unique_type_group_data_set_name,
-                            spark=True, set_i_col=False) \
+                            spark=True, set_i_col=False, set_t_col=True) \
                         .filterByPartitionKeys(
                             (DATE_COL,
                              dates))
