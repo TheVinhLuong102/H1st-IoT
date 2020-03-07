9bdf0448479166004b7d7a8f4f0098008b6f95a8
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-03-07 15:44:56 -0800

    refactor deps & setup files

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-from setuptools import find_packages, setup
-import six
-
-
-install_requires = []
-for s in open('requirements.txt').readlines():
-    if not s.startswith('#'):
-        s = s.strip()
-        lower_s = s.lower()
-        install_requires.append(
-            'Django >= 1.11.29, < 2'   # last 1.x ver compat w/ Py2
-            if lower_s.startswith('django ')
-            else ('Django-AutoComplete-Light >= 3.2.10, < 3.3'   # last 3.2.x ver compat w/ Py2
-                  if six.PY2 and lower_s.startswith('django-autocomplete-light')
-                  else ('DjangoRESTFramework-Filters >= 0.11.1, < 1'   # last 0.x ver compat w/ Py2
-                        if six.PY2 and lower_s.startswith('djangorestframework-filters')
-                        else ('DjangoRESTFramework >= 3.9.4'   # last 3.9 ver compat w/ Py2
-                              if six.PY2 and lower_s.startswith('djangorestframework')
-                              else ('Django-Filter >= 1.1.0, < 2'   # last 1.x ver compat w/ Py2
-                                    if six.PY2 and lower_s.startswith('django-filter')
-                                    else ('Django-Silk >= 3.0.4, < 4'   # last 3.x ver compat w/ Py2
-                                          if six.PY2 and lower_s.startswith('django-silk')
-                                          else s))))))
+from setuptools import find_namespace_packages, setup
 
 
 setup(
@@ -32,7 +10,11 @@ setup(
     description='Arimo Predictive Maintenance',
     long_description='Arimo Predictive Maintenance',
     keywords='Arimo Predictive Maintenance',
-    packages=find_packages(),
+    packages=find_namespace_packages(include=['arimo.*']),
     include_package_data=True,
-    install_requires=install_requires,
+    install_requires=
+        [s for s in
+            {i.strip()
+             for i in open('requirements.txt').readlines()}
+         if not s.startswith('#')],
     scripts=['bin/arimo-pm'])
