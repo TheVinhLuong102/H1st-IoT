5c7e17a802dc385ee04d7a372aa2f96b50b26942
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-11-17 15:38:28 -0800

    refactor h1st_iot.data_mgmt.base.autocompletes

--- h1st_iot/data_mgmt/base/autocompletes.py
+++ h1st_iot/data_mgmt/base/autocompletes.py
@@ -1,131 +1,153 @@
-from dal import autocomplete   # *** DON'T IMPORT SPECIFIC ITEMS INSIDE autocomplete AS THEY CHANGE BETWEEN VERSIONS ***
+"""H1st IoT Data Management (Base) Autocompletes."""
 
-from .models import \
-    NumericMeasurementUnit, \
-    EquipmentGeneralType, \
-    EquipmentComponent, \
-    EquipmentDataField, \
-    EquipmentUniqueTypeGroup, \
-    EquipmentUniqueType, \
-    EquipmentFacility, \
-    EquipmentInstance
+
+# *** DON'T IMPORT SPECIFIC ITEMS INSIDE autocomplete
+# AS THEY CHANGE BETWEEN VERSIONS ***
+from dal import autocomplete
+
+from h1st_iot.data_mgmt.base.models import (
+    NumericMeasurementUnit,
+    EquipmentGeneralType,
+    EquipmentComponent,
+    EquipmentDataField,
+    EquipmentUniqueTypeGroup,
+    EquipmentUniqueType,
+    EquipmentFacility,
+    EquipmentInstance,
+)
 
 
 class NumericMeasurementUnitAutoComplete(autocomplete.Select2QuerySetView):
+    """NumericMeasurementUnitAutoComplete."""
+
     name = 'NumericMeasurementUnit-AutoComplete'
 
     def get_queryset(self):
+        """Queryset."""
         if self.request.user.is_authenticated:
             query_set = NumericMeasurementUnit.objects.all()
 
-            return query_set.filter(name__icontains=self.q) \
-                if self.q \
-              else query_set
+            return (query_set.filter(name__icontains=self.q)
+                    if self.q
+                    else query_set)
 
-        else:
-            return NumericMeasurementUnit.objects.none()
+        return NumericMeasurementUnit.objects.none()
 
 
 class EquipmentGeneralTypeAutoComplete(autocomplete.Select2QuerySetView):
+    """EquipmentGeneralTypeAutoComplete."""
+
     name = 'EquipmentGeneralType-AutoComplete'
 
     def get_queryset(self):
+        """Queryset."""
         if self.request.user.is_authenticated:
             query_set = EquipmentGeneralType.objects.all()
 
-            return query_set.filter(name__icontains=self.q) \
-                if self.q \
-              else query_set
+            return (query_set.filter(name__icontains=self.q)
+                    if self.q
+                    else query_set)
 
-        else:
-            return EquipmentGeneralType.objects.none()
+        return EquipmentGeneralType.objects.none()
 
 
 class EquipmentComponentAutoComplete(autocomplete.Select2QuerySetView):
+    """EquipmentComponentAutoComplete."""
+
     name = 'EquipmentComponent-AutoComplete'
 
     def get_queryset(self):
+        """Queryset."""
         if self.request.user.is_authenticated:
             query_set = EquipmentComponent.objects.all()
 
-            return query_set.filter(name__icontains=self.q) \
-                if self.q \
-              else query_set
+            return (query_set.filter(name__icontains=self.q)
+                    if self.q
+                    else query_set)
 
-        else:
-            return EquipmentComponent.objects.none()
+        return EquipmentComponent.objects.none()
 
 
 class EquipmentDataFieldAutoComplete(autocomplete.Select2QuerySetView):
+    """EquipmentDataFieldAutoComplete."""
+
     name = 'EquipmentDataField-AutoComplete'
 
     def get_queryset(self):
+        """Queryset."""
         if self.request.user.is_authenticated:
             query_set = EquipmentDataField.objects.all()
 
-            return query_set.filter(name__icontains=self.q) \
-                if self.q \
-              else query_set
+            return (query_set.filter(name__icontains=self.q)
+                    if self.q
+                    else query_set)
 
-        else:
-            return EquipmentDataField.objects.none()
+        return EquipmentDataField.objects.none()
 
 
 class EquipmentUniqueTypeGroupAutoComplete(autocomplete.Select2QuerySetView):
+    """EquipmentUniqueTypeGroupAutoComplete."""
+
     name = 'EquipmentUniqueTypeGroup-AutoComplete'
 
     def get_queryset(self):
+        """Queryset."""
         if self.request.user.is_authenticated:
             query_set = EquipmentUniqueTypeGroup.objects.all()
 
-            return query_set.filter(name__icontains=self.q) \
-                if self.q \
-              else query_set
+            return (query_set.filter(name__icontains=self.q)
+                    if self.q
+                    else query_set)
 
-        else:
-            return EquipmentUniqueTypeGroup.objects.none()
+        return EquipmentUniqueTypeGroup.objects.none()
 
 
 class EquipmentUniqueTypeAutoComplete(autocomplete.Select2QuerySetView):
+    """EquipmentUniqueTypeAutoComplete."""
+
     name = 'EquipmentUniqueType-AutoComplete'
 
     def get_queryset(self):
+        """Queryset."""
         if self.request.user.is_authenticated:
             query_set = EquipmentUniqueType.objects.all()
 
-            return query_set.filter(name__icontains=self.q) \
-                if self.q \
-              else query_set
+            return (query_set.filter(name__icontains=self.q)
+                    if self.q
+                    else query_set)
 
-        else:
-            return EquipmentUniqueType.objects.none()
+        return EquipmentUniqueType.objects.none()
 
 
 class EquipmentFacilityAutoComplete(autocomplete.Select2QuerySetView):
+    """EquipmentFacilityAutoComplete."""
+
     name = 'EquipmentFacility-AutoComplete'
 
     def get_queryset(self):
+        """Queryset."""
         if self.request.user.is_authenticated:
             query_set = EquipmentFacility.objects.all()
 
-            return query_set.filter(name__icontains=self.q) \
-                if self.q \
-              else query_set
+            return (query_set.filter(name__icontains=self.q)
+                    if self.q
+                    else query_set)
 
-        else:
-            return EquipmentFacility.objects.none()
+        return EquipmentFacility.objects.none()
 
 
 class EquipmentInstanceAutoComplete(autocomplete.Select2QuerySetView):
+    """EquipmentInstanceAutoComplete."""
+
     name = 'EquipmentInstance-AutoComplete'
 
     def get_queryset(self):
+        """Queryset."""
         if self.request.user.is_authenticated:
             query_set = EquipmentInstance.objects.all()
 
-            return query_set.filter(name__icontains=self.q) \
-                if self.q \
-              else query_set
+            return (query_set.filter(name__icontains=self.q)
+                    if self.q
+                    else query_set)
 
-        else:
-            return EquipmentInstance.objects.none()
+        return EquipmentInstance.objects.none()
