17ecd97c444ea6179baeab8adfb7ba2a6182daa2
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-11-21 21:59:44 -0800

    refactor h1st_iot.data_mgmt.util

--- h1st_iot/data_mgmt/util.py
+++ h1st_iot/data_mgmt/util.py
@@ -1,26 +1,45 @@
+"""H1st IoT Data Management Utilities."""
+
+
 import re
+import sys
+
+if sys.version_info >= (3, 9):
+    from collections.abc import Sequence
+else:
+    from typing import Sequence
+
+
+__all__: Sequence[str] = (
+    'MAX_CHAR_LEN',
+    '_JSON_FMT', '_JSON_EXT',
+    '_PARQUET_FMT', '_PARQUET_EXT',
+    'clean_lower_str', 'clean_upper_str',
+)
 
 
 MAX_CHAR_LEN = 255
 
 
 _JSON_FMT = 'json'
-_JSON_EXT = '.{}'.format(_JSON_FMT)
+_JSON_EXT = f'.{_JSON_FMT}'
 
 _PARQUET_FMT = 'parquet'
-_PARQUET_EXT = '.{}'.format(_PARQUET_FMT)
-
-_YAML_FMT = 'yaml'
-_YAML_EXT = '.{}'.format(_YAML_FMT)
+_PARQUET_EXT = f'.{_PARQUET_FMT}'
 
 
-def _clean_str(s):
-    return re.sub('_{2,}', '_', re.sub('[^\w]+', '_', s).strip('_'))
+# pylint: disable=invalid-name
 
 
-def clean_lower_str(s):
+def _clean_str(s: str) -> str:
+    return re.sub('_{2,}', '_', re.sub(r'[^\w]+', '_', s).strip('_'))
+
+
+def clean_lower_str(s: str) -> str:
+    """Clean & lower-case a string."""
     return _clean_str(s).lower()
 
 
-def clean_upper_str(s):
+def clean_upper_str(s: str) -> str:
+    """Clean & upper-case a string."""
     return _clean_str(s).upper()
